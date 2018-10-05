80864e677940b721f0b8cef8af03425fa433fac7
Corey Lutrick <coreylutrick@gmail.com>
2018-10-04 19:05:22 -0700

    got inmates returning by interests, have to change data structure tough

--- CLinkedIn/Controllers/InmatesController.cs
+++ CLinkedIn/Controllers/InmatesController.cs
@@ -18,9 +18,22 @@ namespace CLinkedIn.Controllers
         {
             Inmate = new List<Inmates>
             {
-                new Inmates { Id = 3, Name = "Jerry", IsMember = true, Interests = new Interests { Type = InterestType.EatingCheezItsByTheBox}, Services = new Services {Type = ServiceType.SnuggleBuddy }, Gender = Inmates.Sex.Male  },
-                new Inmates { Id = 4, Name = "Penelope", IsMember = true, Interests = new Interests { Type = InterestType.HeavyBreathing}, Services = new Services {Type = ServiceType.Smuggler }, Gender = Inmates.Sex.Female  }
+                new Inmates { Id = 0, Name = "Jerry", IsMember = true, Interests = Interests.EatingCheezItsByTheBox, Services = new Services {Type = ServiceType.SnuggleBuddy }, Gender = Inmates.Sex.Male  },
+                new Inmates { Id = 4, Name = "Penelope", IsMember = true, Interests = Interests.HeavyBreathing, Services = new Services {Type = ServiceType.Smuggler }, Gender = Inmates.Sex.Female  }
             };
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Inmates>> GetAll()
+        {
+            return Inmate;
+        }
+ 
+        [HttpGet("CheezIts")]
+        public ActionResult<IEnumerable<Interests>> GetInmatesInterestedInCheezIts()
+        {
+            var cheezItsInmate = Inmate.Where(inmate => inmate.Interests == Interests.EatingCheezItsByTheBox);
+            return Ok(cheezItsInmate);
         }
     }
 }
