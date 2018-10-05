1f0a8c5f5b359c7f38993e493b8ee27ae0caa3bb
Corey Lutrick <coreylutrick@gmail.com>
2018-10-04 19:26:56 -0700

    changed the data from being housed in our inmate controller to the inmate storage so we can then use it to make sure our inmates are getting proper ids

--- CLinkedIn/Controllers/InmatesController.cs
+++ CLinkedIn/Controllers/InmatesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using CLinkedIn.Models;
+using CLinkedIn.DataAccess;
 
 namespace CLinkedIn.Controllers
 {
@@ -12,27 +13,37 @@ namespace CLinkedIn.Controllers
     [ApiController]
     public class InmatesController : ControllerBase
     {
-        static List<Inmates> Inmate;
+        //static List<Inmates> Inmate;
 
-        static InmatesController()
+        //static InmatesController()
+        //{
+        //Inmate = new List<Inmates>
+        //{
+        //new Inmates { Id = 0, Name = "Jerry", IsMember = true, Interests = Interests.EatingCheezItsByTheBox, Services = new Services {Type = ServiceType.SnuggleBuddy }, Gender = Inmates.Sex.Male  },
+        //new Inmates { Id = 4, Name = "Penelope", IsMember = true, Interests = Interests.HeavyBreathing, Services = new Services {Type = ServiceType.Smuggler }, Gender = Inmates.Sex.Female  }
+        //};
+        //}
+
+        private readonly InmateStorage _inmates;
+
+        public InmatesController()
         {
-            Inmate = new List<Inmates>
-            {
-                new Inmates { Id = 0, Name = "Jerry", IsMember = true, Interests = Interests.EatingCheezItsByTheBox, Services = new Services {Type = ServiceType.SnuggleBuddy }, Gender = Inmates.Sex.Male  },
-                new Inmates { Id = 4, Name = "Penelope", IsMember = true, Interests = Interests.HeavyBreathing, Services = new Services {Type = ServiceType.Smuggler }, Gender = Inmates.Sex.Female  }
-            };
+            _inmates = new InmateStorage();
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Inmates>> GetAll()
         {
-            return Inmate;
+            var storage = new InmateStorage();
+            var inmates = storage.GetAllInmates();
+            return Ok(inmates);
         }
  
         [HttpGet("CheezIts")]
         public ActionResult<IEnumerable<Interests>> GetInmatesInterestedInCheezIts()
         {
-            var cheezItsInmate = Inmate.Where(inmate => inmate.Interests == Interests.EatingCheezItsByTheBox);
+            var storage = new InmateStorage();
+            var cheezItsInmate = storage.GetAllInmates().Where(inmate => inmate.Interests == Interests.EatingCheezItsByTheBox);
             return Ok(cheezItsInmate);
         }
     }
